45afd39749b68d5507b3d874b884fe6d6f63346d
Remag29 <tristan.lucas.29@orange.fr>
2024-07-12 16:50:39 +0200

    add score torrent

--- app/routes.py
+++ app/routes.py
@@ -12,6 +12,16 @@ QB_USERNAME = os.getenv('QB_USERNAME', 'ton_username')
 QB_PASSWORD = os.getenv('QB_PASSWORD', 'ton_password')
 
 
+def convert_size_humanreadable(taille_en_bytes: int) -> str:
+    taille_en_gib = taille_en_bytes / (1024 ** 3)
+    taille_en_mib = taille_en_bytes / (1024 ** 2)
+
+    if taille_en_gib >= 1:
+        return f"{taille_en_gib:.2f} Gib"
+    else:
+        return f"{taille_en_mib:.2f} Mib"
+
+
 @main.route('/')
 def index():
     try:
@@ -48,11 +58,13 @@ def index():
         torrents = [{
             'name': torrent['name'],
             # Convertir la taille de bytes en Go (1 Go = 2^30 bytes)
-            'size': f"{torrent['total_size'] / (1024 ** 3):.2f} Go",
+            'size': convert_size_humanreadable(torrent['size']),
             # Convertir le temps actif de secondes en jours (1 jour = 86400 secondes)
             'time_active': f"{torrent['time_active'] / 86400:.2f} jours",
             # Convertir uploadé de bytes en Go
-            'uploaded': f"{torrent['uploaded'] / (1024 ** 3):.2f} Go"
+            'uploaded': convert_size_humanreadable(torrent['uploaded']),
+            # Calcule du score torrent
+            'score_torrent': f"{(torrent['uploaded'] / torrent['size']) / (torrent['time_active'] / 86400) if torrent['time_active'] > 0 else 0:.4f}"
         } for torrent in response.json()]
         return render_template('index.html', torrents=torrents)
 
